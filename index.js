94ee761926258aeb6d073bbaf7a71a4aee2af9f2
timeam <37603653+timeam@users.noreply.github.com>
2021-05-30 00:16:56 +1000

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,5 @@
 var express = require('express');
-var request = require('request');
 var moment = require('moment');
-const cheerio = require('cheerio');
 const jsesc = require('jsesc');
 const https = require('https');
 const path = require('path');
@@ -14,17 +12,9 @@ const END_TIME = Number(process.env.START_TIME);
 
 var app = express();
 var my_array = new Array();
-var permalink_path_array = new Array();
 var ONE_HOUR = 60 * 60 * 1000; /* ms */
-var USER_AGENT = 'Mozilla/5.0 (Windows NT 10.0; Trident/7.0; rv:11.0) like Gecko';
 var source_token = "";
 var refresh = "";
-
-var customHeaderRequest = request.defaults({	
-  headers: {	
-    'User-Agent': USER_AGENT	
-  }	
-});
 
 app.use(bodyParser.urlencoded({extended: true})) 
 app.use(bodyParser.json()) 
@@ -52,12 +42,11 @@ app.listen(process.env.PORT || 3000);
 
 async function processData(stringData) {
   try {
-
     var links = stringData.match(/[A-Za-z]{2,3}\?tag=.*?platform=[a-zA-Z0-9_\-]*/ig);
     if (links != null){
       for( i=0; i<links.length; i++ ) {
-        var link = process.env.URL_FILTER + links[i].replaceAll("amp;", "");
-        if (link.length <= 100 && my_array.indexOf(link) == -1) {
+        var link = process.env.URL_FILTER + links[i].replace(/amp;/g, "");
+        if (link.length <= 100 && my_array.indexOf(link) == -1) {          
           my_array.push(link)
         }
       }
@@ -65,7 +54,7 @@ async function processData(stringData) {
   } catch (e) {console.log(e)}
 }
 
-setInterval(function() {
+setInterval(async function() {
   var temp_obj = my_array.pop();
   if (temp_obj != undefined) {
     var temp_data_expanded_url = temp_obj;
@@ -81,166 +70,150 @@ setInterval(function() {
         var goqrme = encodeURI(process.env.QR_URL) + encodeURIComponent(temp_data_expanded_url);
         var player_post =
           "<" + encodeURI(process.env.QUERY_URL).replace('QUERY',  encodeURIComponent(temp_data_expanded_url.replace("https://", ""))) + ">";
-        const ra_options = {
-          hostname: process.env.RA_HOSTNAME,
-          port: 443,
-          path: process.env.RA_PATH + hashtag,
-          method: 'GET',
-          headers: {
-            'User-Agent': USER_AGENT
+  
+        try {
+          var url = process.env.WEBHOOK_FILTERED_URL;
+          var player_name = "N/A";
+          var trophies = "N/A";
+          var highest_trophies = 0;
+          var best_season = "N/A";
+          var best_season_rank = "N/A";
+          var best_season_date = "N/A";
+          var previous_season = "N/A";
+          var previous_season_best = "";
+          var max_wins = "N/A";
+          var cards_won = "N/A";
+          var experience = "N/A";
+
+          const API_players_response = await got(process.env.API_URL_DOMAIN +"/v1/players/%23" + hashtag, {
+            headers: {
+              "Accept":"application/json",
+              "authorization":"Bearer " + process.env.API_BEARER
+            }
+          });
+
+          if (API_players_response.statusCode == 200) {
+            var api_json_data = JSON.parse(API_players_response.body);
+            if ('name' in api_json_data) {
+              player_name = api_json_data['name'];
+            }
+            if ('trophies' in api_json_data) {
+              trophies = api_json_data['trophies'];
+            }
+            if ('bestTrophies' in api_json_data) {
+              highest_trophies = api_json_data['bestTrophies'];
+            } else {
+              url = process.env.WEBHOOK_UNKNOWN_URL;
+            }  
+            if ('leagueStatistics' in api_json_data){
+              leagueStatistics = api_json_data['leagueStatistics'];
+              if ('previousSeason' in leagueStatistics) {
+                previousSeason = leagueStatistics['previousSeason'];
+                if ('trophies' in previousSeason) {
+                  previous_season = previousSeason['trophies'];
+                }
+                if ('bestTrophies' in previousSeason) {
+                  previous_season_best = " / " + previousSeason['bestTrophies'];
+                }
+              }
+              if ('bestSeason' in leagueStatistics) {
+                bestSeason = leagueStatistics['bestSeason'];
+                if ('trophies' in bestSeason) {
+                  best_season = bestSeason['trophies'];
+                }
+                if ('rank' in bestSeason) {
+                  best_season_rank = bestSeason['rank'];
+                }
+                if ('id' in bestSeason) {
+                  best_season_date = bestSeason['id'];
+                }
+              }
+            }
+            if ('challengeMaxWins' in api_json_data) {
+              max_wins = api_json_data['challengeMaxWins'];
+            }
+            if ('challengeCardsWon' in api_json_data) {
+              cards_won = api_json_data['challengeCardsWon'];
+            }
+            if ('expLevel' in api_json_data) {
+              experience = api_json_data['expLevel'];
+            }
+          } else {
+            console.log(API_players_response.statusCode);
           }
-        };
-        const ra_req = https.request(ra_options, (res) => {
-          res.setEncoding("utf8"); // makes sure that "chunk" is a string.
-          let raFullBody = "";
-          res.on("data", data => {
-            raFullBody += data;
-          });
-          res.on("end", () => {
-            try {
-              var url = process.env.WEBHOOK_FILTERED_URL;
-              var player_name = "N/A";
-              var trophies = "N/A";
-              var highest_trophies = 0;
-              var best_season = "N/A";
-              var best_season_rank = "N/A";
-              var best_season_date = "N/A";
-              var previous_season = "N/A";
-              var previous_season_best = "";
-              var max_wins = "N/A";
-              var cards_won = "N/A";
-              var experience = "N/A";
-              const $ = cheerio.load(raFullBody);
-              var text_header = $('h1.header').first().text();
-              if (typeof text_header !== 'undefined' && text_header !== false && text_header !== "") {
-                player_name = text_header.trim() + ' ';
-              }
-              var text_item = $('div.horizontal').first().children().first().text();
-              if (typeof text_item !== 'undefined' && text_item !== false && text_item !== "") {
-                trophies = text_item.trim().replace(/\s{2,}/g, ' ');
-                var numbers = trophies.match(/\d+/g).map(Number);
-                for( i=0; i<numbers.length; i++ ) {
-                  if (numbers[i] > highest_trophies)
-                    highest_trophies = numbers[i];
-                }
-              } else {
-                url = process.env.WEBHOOK_UNKNOWN_URL;
-              }
-              var text_td_best_season_rank = $("img[src$='rank.png']").first().parent().find('tr').eq(2).find('td').eq(1).text();
-              if (typeof text_td_best_season_rank !== 'undefined' && text_td_best_season_rank !== false && text_td_best_season_rank !== "") {
-                best_season_rank = text_td_best_season_rank.trim().replace(/\s{2,}/g, ' ');
-              }
-              var text_td_best_season = $("img[src$='rank.png']").first().parent().find('tr').eq(3).find('td').eq(1).text();
-              if (typeof text_td_best_season !== 'undefined' && text_td_best_season !== false && text_td_best_season !== "") {
-                best_season = text_td_best_season.trim().replace(/\s{2,}/g, ' ');
-              }
-              var text_td_best_season_date = $("img[src$='rank.png']").first().parent().find('tr').eq(4).find('td').eq(1).text();
-              if (typeof text_td_best_season_date !== 'undefined' && text_td_best_season_date !== false && text_td_best_season_date !== "") {
-                best_season_date = text_td_best_season_date.trim().replace(/\s{2,}/g, ' ');
-              }
-              var text_td_previous_season = $("img[src$='rank.png']").first().parent().find('tr').eq(-2).find('td').eq(1).text();
-              if (typeof text_td_previous_season !== 'undefined' && text_td_previous_season !== false && text_td_previous_season !== "") {
-                previous_season = text_td_previous_season.trim().replace(/\s{2,}/g, ' ');
-              }
-              var text_td_previous_season_best = $("img[src$='rank.png']").first().parent().find('tr').eq(-1).find('td').eq(1).text();
-              if (typeof text_td_previous_season_best !== 'undefined' && text_td_previous_season_best !== false && text_td_previous_season_best !== "") {
-                previous_season_best = " / " + text_td_previous_season_best.trim().replace(/\s{2,}/g, ' ');
-              }
-              var text_td_max_wins = $("img[src$='tournament.png']").first().parent().find('tr').eq(1).find('td').eq(1).text();
-              if (typeof text_td_max_wins !== 'undefined' && text_td_max_wins !== false && text_td_max_wins !== "") {
-                max_wins = text_td_max_wins.trim().replace(/\s{2,}/g, ' ');
-              }
-              var text_td_cards_won = $("img[src$='tournament.png']").first().parent().find('tr').eq(2).find('td').eq(1).text();
-              if (typeof text_td_cards_won !== 'undefined' && text_td_cards_won !== false && text_td_cards_won !== "") {
-                cards_won = text_td_cards_won.trim().replace(/\s{2,}/g, ' ');
-              }
-              var text_td_experience = $("img[src$='cards.png']").first().parent().find('tr').eq(0).find('td').eq(1).text();
-              if (typeof text_td_experience !== 'undefined' && text_td_experience !== false && text_td_experience !== "") {
-                experience = text_td_experience.trim().replace(/\s{2,}/g, ' ');
-              }
-              if (trophies.includes('7,') || trophies.includes('8,') || trophies.includes('N/A') || highest_trophies >= 7000 || DEBUG_MODE) {
-                if (DEBUG_MODE) {
-                  url = process.env.WEBHOOK_DEBUG_URL;
-                }
-                request({
-                  url: url,
-                  method: "POST",
-                  json: {
-                    "username": "Tweet",
-                    "avatar_url": "https://i.imgur.com/q3iAY1B.png",
-                    "embeds": [{
-                      "author": {
-                        "name": player_name + "#" + hashtag,
-                        "icon_url": "https://i.imgur.com/nMRazCT.png"
-                      },
-                      "color": 5746931,
-                      "fields": [{
-                          "name": "Trophies",
-                          "value": trophies
-                        },
-                        {
-                          "name": "Best Season Rank",
-                          "value": best_season_rank
-                        },
-                        {
-                          "name": "Best Season Trophies",
-                          "value": best_season
-                        },
-                        {
-                          "name": "Best Season Date",
-                          "value": best_season_date
-                        },
-                        {
-                          "name": "Previous Season",
-                          "value": previous_season + previous_season_best
-                        },
-                        {
-                          "name": "Max Wins",
-                          "value": max_wins
-                        },
-                        {
-                          "name": "Cards Won",
-                          "value": cards_won
-                        },
-                        {
-                          "name": "Experience",
-                          "value": experience
-                        },
-                        {
-                          "name": "Player Profile",
-                          "value": player_profile
-                        },
-                        {
-                          "name": "Post",
-                          "value": player_post
-                        },
-                        {
-                          "name": "Friend Link",
-                          "value": friend_link
-                        }
-                      ],
-                      "image": {
-                        "url": goqrme
-                      },
-                      "footer": {
-                        "text": moment(new Date((new Date).getTime())).format('YYYY-MM-DD[T]HH:mm:ss.SSS') + "Z"
-                      }
-                    }]
+      
+          if (highest_trophies >= 7000 || DEBUG_MODE) {
+            if (DEBUG_MODE) {
+              url = process.env.WEBHOOK_DEBUG_URL;
+            }
+            const body = await got.post(url, {
+              json: {
+                "username": "Tweet",
+                "avatar_url": "https://i.imgur.com/q3iAY1B.png",
+                "embeds": [{
+                  "author": {
+                    "name": player_name + "#" + hashtag,
+                    "icon_url": "https://i.imgur.com/nMRazCT.png"
+                  },
+                  "color": 5746931,
+                  "fields": [{
+                      "name": "Trophies",
+                      "value": trophies
+                    },
+                    {
+                      "name": "Best Season Rank",
+                      "value": best_season_rank
+                    },
+                    {
+                      "name": "Best Season Trophies",
+                      "value": best_season
+                    },
+                    {
+                      "name": "Best Season Date",
+                      "value": best_season_date
+                    },
+                    {
+                      "name": "Previous Season",
+                      "value": previous_season + previous_season_best
+                    },
+                    {
+                      "name": "Max Wins",
+                      "value": max_wins
+                    },
+                    {
+                      "name": "Cards Won",
+                      "value": cards_won
+                    },
+                    {
+                      "name": "Experience",
+                      "value": experience
+                    },
+                    {
+                      "name": "Player Profile",
+                      "value": player_profile
+                    },
+                    {
+                      "name": "Post",
+                      "value": player_post
+                    },
+                    {
+                      "name": "Friend Link",
+                      "value": friend_link
+                    }
+                  ],
+                  "image": {
+                    "url": goqrme
+                  },
+                  "footer": {
+                    "text": moment(new Date((new Date).getTime())).format('YYYY-MM-DD[T]HH:mm:ss.SSS') + "Z"
                   }
-                }, function(error, response, body) {
-                  if (error) {
-                    console.log(error);
-                    //my_array.unshift(temp_obj);
-                  }
-                });
+                }]
               }
-            } catch (e) {}
-          });
-        });
-        ra_req.on('error', (e) => {
-          console.error(e);
-        });
-        ra_req.end();
+            }).json();
+          }
+        } catch (e) {
+          console.log(e);
+        }
       }
     }
   }
@@ -267,9 +240,13 @@ setInterval(async function() {
             var date_time = new Date(Date.parse(article["created_at"].replace(/( \+)/, ' UTC$1')));
             var h = date_time.getUTCHours();
             if((START_TIME <= h && h < END_TIME && refresh) || DEBUG_MODE) {
+              var items = {};
               for (url in article["entities"]["urls"]) {
-                processData(article["entities"]["urls"][url]["expanded_url"]);
+                items[article["entities"]["urls"][url]["expanded_url"]] = true;
               }
+              for (var i in items) {
+                processData(i);
+              }   
             }
           }
         }
@@ -287,7 +264,7 @@ setInterval(async function() {
           }
           for (entry in entries) {
             if (entries[entry]["entryId"] == "sq-cursor-top" || entries[entry]["entryId"].startsWith("cursor-top-")) {
-                refresh = "&cursor=" + encodeURIComponent(entries[entry]["content"]["operation"]["cursor"]["value"]);
+              refresh = "&cursor=" + encodeURIComponent(entries[entry]["content"]["operation"]["cursor"]["value"]);
             }
           }
         }
@@ -309,20 +286,20 @@ setInterval(async function() {
 }, 30000); // every 30.000 seconds (30000)
 
 
-setInterval(function() {
+setInterval(async function() {
   try {
     var d = new Date((new Date).getTime());	
     var h = d.getUTCHours();
     if (h < 12) {	
-      customHeaderRequest.get(process.env.AM_URL, function(err, resp, body) {});	
+      got(process.env.AM_URL);	
     } else {	
-      customHeaderRequest.get(process.env.PM_URL, function(err, resp, body) {});	
+      got(process.env.PM_URL);	
     }	
     if (h == 23) {	
-      customHeaderRequest.get(process.env.AM_URL, function(err, resp, body) {});	
+      got(process.env.AM_URL);	
     }	
     if (h == 11) {	
-      customHeaderRequest.get(process.env.PM_URL, function(err, resp, body) {});	
+      got(process.env.PM_URL);	
     }
   } catch (error) {
     console.log(error);
