070e59abaa437623c38ec4828bc0c89cd23a2dfb
timeam <37603653+timeam@users.noreply.github.com>
2021-05-29 17:13:59 +1000

    Update index.js

--- index.js
+++ index.js
@@ -4,104 +4,83 @@ var moment = require('moment');
 const cheerio = require('cheerio');
 const jsesc = require('jsesc');
 const https = require('https');
+const path = require('path');
+var bodyParser = require('body-parser');
+const got = require("got");
 
 const DEBUG_MODE = (process.env.DEBUG.toUpperCase() === "TRUE");
+const START_TIME = Number(process.env.START_TIME);
+const END_TIME = Number(process.env.START_TIME);
 
 var app = express();
-var min_position = "";
 var my_array = new Array();
 var permalink_path_array = new Array();
 var ONE_HOUR = 60 * 60 * 1000; /* ms */
 var USER_AGENT = 'Mozilla/5.0 (Windows NT 10.0; Trident/7.0; rv:11.0) like Gecko';
+var source_token = "";
+var refresh = "";
 
-var customHeaderRequest = request.defaults({
-  headers: {
-    'User-Agent': USER_AGENT
-  }
-})
+var customHeaderRequest = request.defaults({	
+  headers: {	
+    'User-Agent': USER_AGENT	
+  }	
+});
+
+app.use(bodyParser.urlencoded({extended: true})) 
+app.use(bodyParser.json()) 
 
 app.get('/', function(req, res) {
-  if (min_position) {
-    res.send((new Date).toISOString() + ' ' + min_position);
+  if (source_token) {
+    res.send((new Date).toISOString() + ' ' + source_token);
   } else {
     res.send((new Date).toISOString() + ' waiting...');
   }
 });
 
+app.get('/subscribe', function (req, res) {
+  res.sendFile(path.join(__dirname + '/subscribe.html'));
+});
+
+app.post('/subscription', function(req, res){
+  if (req.body.email) {
+    processData(req.body.email);
+  }
+  res.redirect('/');
+});
+
 app.listen(process.env.PORT || 3000);
 
-setInterval(function() {
-  const update_options = {
-    hostname: process.env.UPDATE_HOSTNAME,
-    port: 443,
-    path: process.env.UPDATE_PATH,
-    method: 'GET',
-    headers: {
-      'User-Agent': USER_AGENT
+async function processData(stringData) {
+  try {
+
+    var links = stringData.match(/[A-Za-z]{2,3}\?tag=.*?platform=[a-zA-Z0-9_\-]*/ig);
+    if (links != null){
+      for( i=0; i<links.length; i++ ) {
+        var link = process.env.URL_FILTER + links[i].replaceAll("amp;", "");
+        if (link.length <= 100 && my_array.indexOf(link) == -1) {
+          my_array.push(link)
+        }
+      }
     }
-  };
-  const update_req = https.request(update_options, (res) => {
-    res.setEncoding("utf8"); // makes sure that "chunk" is a string.
-    let updateFullBody = "";
-    res.on("data", data => {
-      updateFullBody += data;
-    });
-    res.on("end", () => {
-      try {
-        const $ = cheerio.load(updateFullBody);
-        $('.timeline-item').each(function(i, elem) {
-          var permalink_path = $(this).find(".tweet-link").attr("href");
-          var data_screen_name = $(this).find(".fullname-and-username .username").attr("href");
-          var datestring = $(this).find(".tweet-date a").attr("title");
-          var parts = datestring.match(/(\d{1,2})\/(\d{1,2})\/(\d{4}), (\d{1,2}):(\d{2}):(\d{2})/); //19/9/2020, 12:33:38
-          var data_time_ms = Date.UTC(+parts[3], +parts[2] - 1, +parts[1], +parts[4], +parts[5], +parts[6]);
-          if (typeof permalink_path !== 'undefined' && permalink_path !== false && permalink_path !== "" && permalink_path_array.indexOf(permalink_path) == -1) {
-            permalink_path_array.push(permalink_path);
-            var items = {};
-            $(this).find('.tweet-content a').each(function(index, element) {
-              var data_expanded_url = $(element).attr('href');
-              if (typeof data_expanded_url !== typeof undefined && data_expanded_url !== false) {
-                items[data_expanded_url] = true;
-              }
-            });
-            for (var i in items) {
-              my_array.push({
-                permalink_path: permalink_path,
-                data_screen_name: data_screen_name,
-                data_time_ms: data_time_ms,
-                data_expanded_url: i
-              });
-            }
-          }
-        });
-      } catch (e) {}
-    });
-  });
-  update_req.on('error', (e) => {
-    console.error(e);
-  });
-  update_req.end();
-}, 30000); // every 30 seconds (30000)
+  } catch (e) {console.log(e)}
+}
 
 setInterval(function() {
   var temp_obj = my_array.pop();
   if (temp_obj != undefined) {
-    var temp_data_expanded_url = temp_obj.data_expanded_url;
+    var temp_data_expanded_url = temp_obj;
     if (temp_data_expanded_url.startsWith(process.env.URL_FILTER) && temp_data_expanded_url.includes('tag')) {
-      var d = new Date((new Date).getTime()); //new Date(temp_obj.data_time_ms);
-      var h = d.getHours();
       var re = /.*tag=(.*?)&.*/;
-      var hashtag = temp_obj.data_expanded_url.replace(re, "$1");
-      if (hashtag && hashtag != 'Y8LRCLVC' && h < 12) {
+      var hashtag = temp_data_expanded_url.replace(re, "$1");
+      if (hashtag && hashtag != 'Y8LRCLVC') {
         var player_profile =
           "<" + encodeURI(process.env.SR_URL + hashtag) + ">\n" +
           "<" + encodeURI(process.env.DS_URL + hashtag) + ">\n" +
           "<" + encodeURI(process.env.RA_URL + hashtag) + ">";
-        var friend_link = encodeURI(temp_obj.data_expanded_url);
-        var goqrme = encodeURI(process.env.QR_URL) + encodeURIComponent(temp_obj.data_expanded_url);
+        var friend_link = encodeURI(temp_data_expanded_url);
+        var goqrme = encodeURI(process.env.QR_URL) + encodeURIComponent(temp_data_expanded_url);
         var player_post =
-          "<" + encodeURI(process.env.UPDATE_URL_DOMAIN + temp_obj.data_screen_name) + ">\n" +
-          encodeURI(process.env.UPDATE_URL_DOMAIN + temp_obj.permalink_path);
+          "<" + encodeURI(process.env.QUERY_URL).replace('QUERY',  encodeURIComponent(temp_data_expanded_url.replace("https://", ""))) + ">";
         const ra_options = {
           hostname: process.env.RA_HOSTNAME,
           port: 443,
@@ -119,9 +98,10 @@ setInterval(function() {
           });
           res.on("end", () => {
             try {
-              var url = process.env.WEBHOOK_FILTERED_URL;
+              var url = process.env.WEBHOOK_MANUAL_URL;
               var player_name = "N/A";
               var trophies = "N/A";
+              var highest_trophies = 0;
               var best_season = "N/A";
               var best_season_rank = "N/A";
               var best_season_date = "N/A";
@@ -138,6 +118,11 @@ setInterval(function() {
               var text_item = $('div.horizontal').first().children().first().text();
               if (typeof text_item !== 'undefined' && text_item !== false && text_item !== "") {
                 trophies = text_item.trim().replace(/\s{2,}/g, ' ');
+                var numbers = trophies.match(/\d+/g).map(Number);
+                for( i=0; i<numbers.length; i++ ) {
+                  if (numbers[i] > highest_trophies)
+                    highest_trophies = numbers[i];
+                }
               } else {
                 url = process.env.WEBHOOK_UNKNOWN_URL;
               }
@@ -173,7 +158,7 @@ setInterval(function() {
               if (typeof text_td_experience !== 'undefined' && text_td_experience !== false && text_td_experience !== "") {
                 experience = text_td_experience.trim().replace(/\s{2,}/g, ' ');
               }
-              if (trophies.includes('7,') || trophies.includes('8,') || trophies.includes('N/A') || DEBUG_MODE) {
+              if (trophies.includes('7,') || trophies.includes('8,') || trophies.includes('N/A') || highest_trophies >= 7000 || DEBUG_MODE) {
                 if (DEBUG_MODE) {
                   url = process.env.WEBHOOK_DEBUG_URL;
                 }
@@ -189,7 +174,6 @@ setInterval(function() {
                         "icon_url": "https://i.imgur.com/nMRazCT.png"
                       },
                       "color": 5746931,
-                      "timestamp": moment(temp_obj.data_time_ms).format('YYYY-MM-DD[T]HH:mm:ss.SSS') + "Z",
                       "fields": [{
                           "name": "Trophies",
                           "value": trophies
@@ -239,7 +223,7 @@ setInterval(function() {
                         "url": goqrme
                       },
                       "footer": {
-                        "text": moment(temp_obj.data_time_ms).format('YYYY-MM-DD[T]HH:mm:ss.SSS') + "Z"
+                        "text": moment(new Date((new Date).getTime())).format('YYYY-MM-DD[T]HH:mm:ss.SSS') + "Z"
                       }
                     }]
                   }
@@ -262,18 +246,90 @@ setInterval(function() {
   }
 }, 6457); // every 6.457 seconds (6457)
 
+
+setInterval(async function() {
+  try {
+    if (source_token) {
+      temp_source_json_url = process.env.SOURCE_JSON_URL.replace("typed_query" , "typed_query" + refresh);
+      const temp_source_json_url_response = await got(temp_source_json_url, {
+        headers: {
+            "x-guest-token": source_token,
+            "Authorization": "Bearer " + process.env.SOURCE_BEARER,
+            "Referer": process.env.SOURCE_URL
+        }
+      });
+      var json_data = JSON.parse(temp_source_json_url_response.body);
+      if (json_data.globalObjects && json_data.globalObjects.tweets) {
+          var json_root = json_data["globalObjects"]["tweets"];
+          for (item in json_root) {
+              var article = json_root[item]
+              if (article.entities) {
+                var date_time = new Date(Date.parse(article["created_at"].replace(/( \+)/, ' UTC$1')));
+                var h = date_time.getUTCHours();
+                if(START_TIME <= h && h < END_TIME && refresh) {
+                  var items = {};
+                  for (url in article["entities"]["urls"]) {
+                    if ("expanded_url" in article["entities"]["urls"][url]) {
+                      items[article["entities"]["urls"][url]["expanded_url"]] = true;
+                    }
+                  }
+                  for (var i in items) {
+                    processData(i);
+                  }                            }
+              }
+          }
+      }
+      
+      if (json_data.timeline && json_data.timeline.instructions) {
+          var entries;
+          for (instruction in json_data["timeline"]["instructions"]) {
+              if ("addEntries" in json_data["timeline"]["instructions"][instruction]) {
+                  entries = json_data["timeline"]["instructions"][instruction]["addEntries"]["entries"];
+              } else if ("replaceEntry" in json_data["timeline"]["instructions"][instruction]) {
+                  entries = [json_data["timeline"]["instructions"][instruction]["replaceEntry"]["entry"]];
+              } else {
+                  continue;
+              }
+              for (entry in entries) {
+                  if (entries[entry]["entryId"] == "sq-cursor-top" || entries[entry]["entryId"].startsWith("cursor-top-")) {
+                      refresh = "&cursor=" + encodeURIComponent(entries[entry]["content"]["operation"]["cursor"]["value"]);
+                  }
+              }
+          }
+      }
+    } else {
+      const source_url_response = await got(process.env.SOURCE_URL);
+      var myRegexp = /gt=(\d+)/ig;
+      var match = myRegexp.exec(source_url_response.body);
+      source_token = match[1];
+      if(match.length > 0){
+        source_token = match[1];
+      }
+    }
+  } catch (error) {
+    source_token = "";
+    refresh = "";
+    console.log(error);
+  }
+}, 30000); // every 30.000 seconds (30000)
+
+
 setInterval(function() {
-  var d = new Date((new Date).getTime());
-  var h = d.getHours();
-  if (h < 12) {
-    customHeaderRequest.get(process.env.AM_URL, function(err, resp, body) {});
-  } else {
-    customHeaderRequest.get(process.env.PM_URL, function(err, resp, body) {});
+  try {
+    var d = new Date((new Date).getTime());	
+    var h = d.getUTCHours();
+    if (h < 12) {	
+      customHeaderRequest.get(process.env.AM_URL, function(err, resp, body) {});	
+    } else {	
+      customHeaderRequest.get(process.env.PM_URL, function(err, resp, body) {});	
+    }	
+    if (h == 23) {	
+      customHeaderRequest.get(process.env.AM_URL, function(err, resp, body) {});	
+    }	
+    if (h == 11) {	
+      customHeaderRequest.get(process.env.PM_URL, function(err, resp, body) {});	
+    }
+  } catch (error) {
+    console.log(error);
   }
-  if (h == 23) {
-    customHeaderRequest.get(process.env.AM_URL, function(err, resp, body) {});
-  }
-  if (h == 11) {
-    customHeaderRequest.get(process.env.PM_URL, function(err, resp, body) {});
-  }
-}, 300000); // every 5 minutes (300000)
+}, 300000); // every 5 minutes (300000) */
